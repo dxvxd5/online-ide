4dd0d212f06675c83d59624fe32051d31628e282
Milena <milenavi@kth.se>
2021-09-15 00:28:56 +0200

    Testing with Socket.io and CodeMirror code editor

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,111 +1,38 @@
-import React, { ReactElement, ReactNode } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { Codemirror } from 'react-codemirror-ts';
 import './App.css';
-
-// defaultProps:
-const defaultContainerProps = {
-  // eslint-disable-next-line react/default-props-match-prop-types
-  heading: <strong>My Heading</strong>,
-};
-
-type ContainerProps = {
-  children: ReactNode;
-} & typeof defaultContainerProps;
-
-function Container({ heading, children }: ContainerProps): ReactElement {
-  return (
-    <div>
-      <h1>{heading}</h1>
-      {children}
-    </div>
-  );
-}
-Container.defaultProps = defaultContainerProps;
-
-// Conventional Props: (If you don't want to support children, but only have a title prop)
-function Heading({ title }: { title: string }) {
-  return <h1>{title}</h1>;
-}
-
-function HeadingWithContent({
-  children,
-}: {
-  children: ReactNode;
-}): ReactElement {
-  return <h1>{children}</h1>;
-}
-
-// Adds a defaultprop for children
-const HeadingFC: React.FC<{ title: string }> = ({ title }) => <h1>{title}</h1>;
-
-// Functional props (i.e. properties)
-// with a function property "children"
-// that returns back a ReactNode
-function TextWithNumber({
-  header,
-  children,
-}: {
-  // Make header prop optional:
-  // header?: (num: number) => ReactNode;
-  header: (num: number) => ReactNode;
-  children: (num: number) => ReactNode;
-}) {
-  const [state, stateSet] = React.useState<number>(1);
-  // const [state, stateSet] = React.useState<number | null>(null);
-
-  return (
-    <div>
-      {/* Conditionally rendered: */}
-      {/* {header && <h2>{header?.(state)}</h2>} */}
-      <h2>{header(state)}</h2>
-      <div>{children(state)}</div>
-      <div>
-        <button type="button" onClick={() => stateSet(state + 1)}>
-          Add
-        </button>
-      </div>
-    </div>
-  );
-}
-
-// List Component
-// Calls render prop on every item prop in the list component
-function List<ListItem>({
-  items,
-  render,
-}: // ListItem array is a generic type variable:
-{
-  items: ListItem[];
-  render: (item: ListItem) => ReactNode;
-}) {
-  return (
-    <ul>
-      {items.map((item) => (
-        <li key={1}>{render(item)}</li>
-      ))}
-    </ul>
-  );
-}
+import io from 'socket.io-client';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/addon/edit/matchbrackets';
+import 'codemirror/lib/codemirror.css';
 
 function App(): JSX.Element {
+  const [value, setValue] = useState<string>('');
+  const socketClient = useRef<SocketIOClient.Socket>();
+
+  useEffect(() => {
+    socketClient.current = io.connect('http://localhost:5000');
+
+    if (socketClient.current) {
+      //
+    }
+  });
   return (
-    <div>
-      <Heading title="Hello there" />
-      <HeadingWithContent>
-        <strong>hi!</strong>
-      </HeadingWithContent>
-      <Container>Foo</Container>
-      <HeadingFC title="Hi Again" />
-      <TextWithNumber header={(num: number) => <span>Header {num}</span>}>
-        {(num: number) => (
-          <div>
-            {" Today's number is "} {num}
-          </div>
-        )}
-      </TextWithNumber>
-      <List
-        items={['Jack', 'Sadie', 'Oso']}
-        render={(item: string) => <div>{item.toLowerCase()}</div>}
-      />
+    <div className="app">
+      <Codemirror
+        value=""
+        name="example"
+        options={{
+          lineNumbers: true,
+          lineWrapping: true,
+          matchBrackets: true,
+          mode: 'javascript',
+          tabSize: 2,
+        }}
+        onChange={(value2, options) => {
+          setValue(value2);
+        }}
+      />{' '}
     </div>
   );
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,61 +1,24 @@
-/* import express from 'express';
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
 const PORT = 8000;
+const serverPORT = 5000;
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: 'http://localhost:3000' } });
 
-app.get('/', (req, res) => {
+io.on('connection', (socket) => {
+  console.log('Client connected');
+});
+
+server.listen(serverPORT, () =>
+  console.log(`Server started on port ${serverPORT}!`)
+);
+
+/*app.get('/', (req, res) => {
   res.send('Well done!');
 });
 app.listen(PORT, () => {
   console.log(`The application is listening on port ${PORT}!`);
 });*/
-
-const express = require('express');
-const app = express();
-const path = require('path');
-const PORT = 8000;
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-const bodyParser = require('body-parser');
-
-app.use(bodyParser.json());
-
-//app.use(express.static(`${__dirname}/../build`));
-
-io.sockets.on('connection', (socket: any) => {
-  console.log('user connected');
-
-  socket.on('join', (room: any) => {
-    //room name is just the pathname, split and joined to remove forward slash
-    let fixedRoom = room.split('/').join('');
-    socket.join(room.split('/').join(''));
-    io.in(fixedRoom).emit('room', fixedRoom);
-  });
-
-  socket.on('message', (message: any) => {
-    io.in(message.room).emit('message', {
-      room: message.room,
-      value: message.newValue,
-    });
-  });
-
-  // socket.on('toolbar', (settings) => {
-  //     console.log('settngs', settings)
-  //     io.in(settings.room).emit('toolbar', settings)
-  // })
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-});
-
-app.get('/', (_req: any, res: any) => {
-  //res.sendFile(path.join(__dirname, '../build/index.html'));
-  res.send('Well done!');
-});
-
-server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
-
-app.listen(PORT, () => {
-  console.log(`The application is listening on port ${PORT}!`);
-});
