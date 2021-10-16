e483554dd601a15ff5581add5a236e20ee9cdbc0
dxvxd5 <42718923+dxvxd5@users.noreply.github.com>
2021-10-16 18:01:09 +0200

    minor bug fix

--- client/src/app/ide/ide-header/IdeHeaderPresenter.tsx
+++ client/src/app/ide/ide-header/IdeHeaderPresenter.tsx
@@ -73,7 +73,6 @@ export default function IdeHeaderPresenter({
 
   return (
     <IdeHeaderView
-      copyClipboard={copyClipboard}
       removeCollaborator={removeCollaborator}
       leaveProject={leaveProject}
       startFollowOnClick={startFollowOnClick}
